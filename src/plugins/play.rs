f27da4a43a0e578a107c8c876195984af7b5d4fb
Akatsuki Rui <3736910+akiirui@users.noreply.github.com>
2023-01-17 08:37:36 +0800

    refactor(src/plugins/play.rs): generate option string

--- src/plugins/play.rs
+++ src/plugins/play.rs
@@ -2,21 +2,16 @@ use crate::config::Config;
 use crate::error::Error;
 use crate::protocol::Protocol;
 
-const C_PREFIX: &str = "--ytdl-raw-options-append=cookies=";
-const P_PREFIX: &str = "--profile=";
-const Q_2160: &str = "--ytdl-format=bestvideo[height<=2160]+bestaudio/best[height<=2160]/best";
-const Q_1440: &str = "--ytdl-format=bestvideo[height<=1440]+bestaudio/best[height<=1440]/best";
-const Q_1080: &str = "--ytdl-format=bestvideo[height<=1080]+bestaudio/best[height<=1080]/best";
-const Q_720: &str = "--ytdl-format=bestvideo[height<=720]+bestaudio/best[height<=720]/best";
-const Q_480: &str = "--ytdl-format=bestvideo[height<=480]+bestaudio/best[height<=480]/best";
-const Q_360: &str = "--ytdl-format=bestvideo[height<=360]+bestaudio/best[height<=360]/best";
+const PREFIX_COOKIES: &str = "--ytdl-raw-options-append=cookies=";
+const PREFIX_PROFILE: &str = "--profile=";
+const PREFIX_QUALITY: &str = "--ytdl-format=";
 
 /// Execute player with given options
 pub fn exec(proto: &Protocol, config: &Config) -> Result<(), Error> {
     let mut options: Vec<&str> = Vec::new();
 
     // Append cookies option
-    let mut cookies_option: String;
+    let mut option_cookies: String;
 
     if let Some(v) = proto.cookies {
         let mut p: std::path::PathBuf;
@@ -41,34 +36,40 @@ pub fn exec(proto: &Protocol, config: &Config) -> Result<(), Error> {
         }
 
         if p.exists() {
-            cookies_option = String::from(C_PREFIX);
-            cookies_option.push_str(&p.display().to_string());
-            options.push(&cookies_option);
+            option_cookies = String::from(PREFIX_COOKIES);
+            option_cookies.push_str(&p.display().to_string());
+            options.push(&option_cookies);
         } else {
             eprintln!("Cookies file {v} doesn't exist");
         }
     }
 
     // Append profile option
-    let mut profile_option: String;
+    let mut option_profile: String;
 
     if let Some(v) = proto.profile {
-        profile_option = String::from(P_PREFIX);
-        profile_option.push_str(v);
-        options.push(&profile_option);
+        option_profile = String::from(PREFIX_PROFILE);
+        option_profile.push_str(v);
+        options.push(&option_profile);
     }
 
     // Append quality option
+    let option_quality: String;
+
     if let Some(v) = proto.quality {
-        match v {
-            "2160p" => options.push(Q_2160),
-            "1440p" => options.push(Q_1440),
-            "1080p" => options.push(Q_1080),
-            "720p" => options.push(Q_720),
-            "480p" => options.push(Q_480),
-            "360p" => options.push(Q_360),
-            _ => (),
+        option_quality = match v {
+            "2160p" => quality(2160),
+            "1440p" => quality(1440),
+            "1080p" => quality(1080),
+            "720p" => quality(720),
+            "480p" => quality(480),
+            "360p" => quality(360),
+            _ => String::new(),
         };
+
+        if option_quality.len() != 0 {
+            options.push(&option_quality);
+        }
     };
 
     // Fix some browsers to overwrite "LD_LIBRARY_PATH" on Linux
@@ -96,4 +97,12 @@ pub fn exec(proto: &Protocol, config: &Config) -> Result<(), Error> {
         },
         Err(e) => Err(Error::PlayerRunFailed(e)),
     }
+}
+
+fn quality(height: i32) -> String {
+    let mut option = String::from(PREFIX_QUALITY);
+
+    option.push_str(&format!("bv*[height<={height}]+ba/b[height<={height}]/b"));
+
+    option
 }
